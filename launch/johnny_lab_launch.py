140575b0e38304201e39a4e476a92ca6f010f92d
Fingerling42 <bermanivan42@gmail.com>
2024-05-17 13:15:23 +0300

    Added launch file for localization and navigation with configs

---
+++ launch/johnny_lab_launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    # Prepare config files
+    config_localization = os.path.join(
+        get_package_share_directory('turtlebot4_johnny_lab'),
+        'config',
+        'turtlebot4_localization.yaml'
+    )
+
+    config_navigation = os.path.join(
+        get_package_share_directory('turtlebot4_johnny_lab'),
+        'config',
+        'turtlebot4_nav2.yaml'
+    )
+
+    # Adding launch files from Turtlebot 4 Navigation stack
+    turtlebot4_localization = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('turtlebot4_navigation'), 'launch'),
+            '/localization.launch.py']),
+        launch_arguments={
+            'params': config_localization,
+            'log_level': 'error',
+        }.items()
+    )
+
+    turtlebot4_navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('turtlebot4_navigation'), 'launch'),
+            '/nav2.launch.py']),
+        launch_arguments={
+            'params_file': config_navigation,
+        }.items()
+    )
+
+    return LaunchDescription([
+        turtlebot4_localization,
+        turtlebot4_navigation
+    ])
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'turtlebot4_johnny_lab'
 
@@ -10,16 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='harzroller',
-    maintainer_email='bermanivan42@gmail.com',
+    maintainer='Ivan Berman',
+    maintainer_email='fingerling42@proton.me',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'johnny_lab_navigator = turtlebot4_johnny_lab.johnny_lab_navigator:main',
         ],
     },
 )
